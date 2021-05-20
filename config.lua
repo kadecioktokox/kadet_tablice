5cfa769f6c51dc3b80241eae9279c41bfb9ec899
kadecioktokox <83604056+kadecioktokox@users.noreply.github.com>
2021-05-20 11:13:14 +0200

    kadet_tablice
    kadet_tablice by kadeciok#1234

---
+++ config.lua
@@ -0,0 +1,21 @@
+-- 
+
+Config = {
+	czas = 30, -- Czas po którym widać tablice (minuty)
+	cooldown = 60, -- Czas jaki trzeba czekać aby jeszcze raz użyć zamazywania tablic
+	money = 35000, -- Ile ma kosztować zdjęcia tablic	
+	coords = { x = 268.82, y = -1722.79, z = 29.18 }, -- Koordynaty warsztatu
+
+	-- Blip (https://docs.fivem.net/game-references/blips/)
+	blipEnabled = true, -- Czy ma się wyświetlać blip na mapie?
+	blipSprite = 402,
+	blipColour = 25,
+	blipScale = 1.8,
+	blipName = 'Zamazywanie Tablic',
+	
+	-- Ped (https://wiki.rage.mp/index.php?title=Peds)
+	pedName = 'a_m_m_fatlatin_01',
+	pedHash = 0x61D201B3,
+	pedHeading = 7.0,
+	pedCoords = { x = 270.36, y = -1727.04, z = 29.26 },
+}
---
+++ server.lua
@@ -0,0 +1,17 @@
+ESX = nil
+
+TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+
+-- Na dole masz jak chcesz mieć komende /tablica to usuwasz w 4 linijkach: --
+
+--RegisterCommand("tablica", function(source, args, raw)
+--    local src = source
+--    TriggerClientEvent("foka_tablice:command", src)
+--end)
+
+RegisterServerEvent('foka_tablice:pay')
+AddEventHandler('foka_tablice:pay', function()
+	local _source = source
+	local xPlayer  = ESX.GetPlayerFromId(source)
+	xPlayer.removeMoney(Config.money)
+end)
